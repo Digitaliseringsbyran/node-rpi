969681e5f83c70554705938c6b5dc3bb5367f0d1
David Öhlin <hello@davidohl.in>
2018-03-16 16:33:32 +0100

    run open close on /purchase

--- routes/purchase.js
+++ routes/purchase.js
@@ -1,6 +1,20 @@
+var gpio = require('rpi-gpio');
+
 module.exports = (app, db) => {
 	app.post('/purchase', (req, res) => {
-		console.log(req)
-		res.send(req.body)
+		gpio.setMode(gpio.MODE_BCM);
+		gpio.setup(4, gpio.DIR_HIGH, (err) => {
+			gpio.write(4, true, function(err) {
+				if (err) throw err;
+				console.log('Opened!');
+			});
+
+			setTimeout(() => {
+				gpio.write(4, false, function(err) {
+					if (err) throw err;
+					console.log('Closed!');
+				});
+			}, 1000)
+		});
 	});
 };
